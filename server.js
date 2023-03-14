87da4a9535fe68053bd81883d63738be268ef2f3
monhelle <monhelle@gmail.com>
2023-03-14 11:48:58 +0100

    refactoring code

--- server.js
+++ server.js
@@ -1,44 +1,53 @@
 const { urlencoded } = require("express");
 const express = require("express");
 
-
 const app = express();
-app.use(express.static("public"))
-app.use(express.urlencoded({extended:true}))
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
 
-app.set("view engine", "ejs")
+app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
   res.render("index");
 });
 
 app.post("/api/post/netplan", (req, res) => {
-  console.log('req body')
-  let {elevpool, subnet, address, gateway, nameserver, searchdomain} = req.body;
-  console.log(subnet, address, gateway, nameserver, searchdomain)
+  let { elevpool, subnet, address, gateway, nameserver, searchdomain } =
+    req.body;
+  let message = "";
 
-  if (subnet === `10.12.${elevpool}.0/24`) {
-    console.log("wohooo subnet")
+
+  if (!address.includes(`10.12.${elevpool}`)) {
+    message += "this didnt work, address <br>";
+
   }
 
-  if (address.includes(`10.12.${elevpool}`)) {
-    console.log("wohooo address")
+  if (gateway !== `10.12.${elevpool}.1`) {
+    message += "this didnt work, gateway <br>";
+
   }
 
-  if (gateway === `10.12.${elevpool}.1`) {
-    console.log("wohooo gateway")
+  if (nameserver !== `1.1.1.1` && nameserver !== `8.8.8.8`) {
+    message += "this didnt work, nameserver <br>";
+
   }
 
-  if (nameserver === `1.1.1.1` || nameserver === `8.8.8.8`) {
-    console.log("wohooo nameserver")
+  if (!searchdomain.includes(`ikt-fag.no`)) {
+    message += "this didnt work, searchdomain <br>";
+
   }
 
-  if (searchdomain.includes(`ikt-fag.no`)) {
-    console.log("wohooo searchdomain")
+  if (
+    subnet === `10.12.${elevpool}.0/24` &&
+    address.includes(`10.12.${elevpool}`) &&
+    gateway === `10.12.${elevpool}.1` &&
+    (nameserver === `1.1.1.1` || nameserver === `8.8.8.8`) &&
+    searchdomain.includes(`ikt-fag.no`)
+  ) {
+    console.log("wohooo all works");
+  } else {
+    console.log("this didnt work, redo");
+    res.send(message)
   }
-
-  // if (subnet === `10.12.${32}.0/24`) {
-  //   console.log("wohooo")
-  // }
-})
-app.listen(80) 
+});
+app.listen(80);
