af6361a48b1c02f43e83be5144f5ec8218dd38a0
monhelle <monhelle@gmail.com>
2023-03-14 11:52:12 +0100

    adding another to nameservers

--- server.js
+++ server.js
@@ -16,38 +16,41 @@ app.post("/api/post/netplan", (req, res) => {
     req.body;
   let message = "";
 
-
   if (!address.includes(`10.12.${elevpool}`)) {
     message += "this didnt work, address <br>";
-
   }
 
   if (gateway !== `10.12.${elevpool}.1`) {
     message += "this didnt work, gateway <br>";
-
   }
 
-  if (nameserver !== `1.1.1.1` && nameserver !== `8.8.8.8`) {
+  if (
+    nameserver !== `1.1.1.1` &&
+    nameserver !== `8.8.8.8` &&
+    nameserver !== `1.1.1.1,8.8.8.8` &&
+    nameserver !== `1.1.1.1, 8.8.8.8`
+  ) {
     message += "this didnt work, nameserver <br>";
-
   }
 
   if (!searchdomain.includes(`ikt-fag.no`)) {
     message += "this didnt work, searchdomain <br>";
-
   }
 
   if (
     subnet === `10.12.${elevpool}.0/24` &&
     address.includes(`10.12.${elevpool}`) &&
     gateway === `10.12.${elevpool}.1` &&
-    (nameserver === `1.1.1.1` || nameserver === `8.8.8.8`) &&
+    (nameserver === `1.1.1.1` ||
+      nameserver === `8.8.8.8` ||
+      nameserver === `1.1.1.1,8.8.8.8` ||
+      nameserver === `1.1.1.1, 8.8.8.8`) &&
     searchdomain.includes(`ikt-fag.no`)
   ) {
     console.log("wohooo all works");
   } else {
     console.log("this didnt work, redo");
-    res.send(message)
+    res.send(message);
   }
 });
 app.listen(80);
